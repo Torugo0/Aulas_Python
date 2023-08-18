8ae0ef6f35d988e8437e2c5229c2279ecc5cafab
Vitor Hugo <vitorh565.vh@gmail.com>
2023-08-18 16:00:06 -0300

    -

---
+++ Aulas/Strings_Listas_Tuplas/Tuplas/exercicios/5.py
@@ -0,0 +1,12 @@
+def tamanho_string(palavra):
+    return len(palavra)
+
+frase = True
+
+while frase:
+    palavra = input("Digite uma palavra: ")
+    if (palavra == "0"):
+        frase = False
+    else:
+        print(f"O tamanho da palavra {palavra} é: {tamanho_string(palavra)}")
+    
